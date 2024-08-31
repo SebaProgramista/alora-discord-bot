c906f017d53fac2ad588565cf8b3e382ce6829e0
Sebastian <91561361+SebaProgramista@users.noreply.github.com>
2024-08-31 19:10:30 +0200

    Update bot/logger_manager.py
    Co-authored-by: Bartosz <46005237+valdife@users.noreply.github.com>

--- bot/logger_manager.py
+++ bot/logger_manager.py
@@ -6,11 +6,12 @@ config = json.load(open(".config.json"))
 class LoggerManager:
     def __init__(self):
         logger = logging.getLogger(__name__)
-        logger.setLevel(logging.DEBUG if config["debugMode"] else logging.INFO)
+        logging_level = logging.DEBUG if config["debugMode"] else logging.INFO
+        logger.setLevel(logging_level)
         
         consoleHandler = logging.StreamHandler()
         consoleHandler.setFormatter(logging.Formatter("%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s] %(message)s"))
-        consoleHandler.setLevel(logging.DEBUG if config["debugMode"] else logging.INFO)
+        consoleHandler.setLevel(logging_level)
 
         logger.addHandler(consoleHandler)
 
