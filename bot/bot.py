7477428f812b501951444d302ec648100990160a
Sebastian <91561361+SebaProgramista@users.noreply.github.com>
2025-01-06 17:18:46 +0100

    Add more logger outputs

--- bot/bot.py
+++ bot/bot.py
@@ -31,8 +31,12 @@ class Bot(commands.Bot):
         bot.logger.info(f"{self.user.name} is ready now!")
         for idx, guild in enumerate(self.guilds):
             bot.logger.info(f"{idx+1}. {guild.name}")
-            self.refresh_levels(guild)
-            self.set_voice_join_time(guild)
+            logger.debug(f"[var: 'guild'] {guild}")
+            logger.debug(f"[var: 'guild.id'] {guild.id}")
+            logger.debug(f"[var: 'config[\"serverID\"]'] {config['serverID']}")
+            if guild.id == int(config["serverID"]):
+                self.refresh_levels(guild)
+                self.set_voice_join_time(guild)
     
     # Set voice_join_time for members who are currently on voice
     def set_voice_join_time(self, guild: discord.Guild):
@@ -52,15 +56,18 @@ class Bot(commands.Bot):
     def refresh_levels(self, guild: discord.Guild):
         try:
             levels = session_manager.session.query(session_manager.level).all()
+            logger.debug(f"[var: levels] {levels}")
             for level in levels:
                 session_manager.session.delete(level)
             req = 0
             add = 1000
             for role in self.get_guild(guild.id).roles:
+                logger.debug(f"[var: 'role'] {role}")
                 if "Poziom" in role.name:
                     req += add
                     add *= 1.10
                     level = session_manager.create_new_level(role_id=role.id, required_points=req)
+                    logger.debug(f"[var: 'level'] {level}")
                     session_manager.session.add(level)
         except SQLAlchemyError as e:
             logger.error(e)
